8dc6fd08c0a677dc9cf579cc418a61a1f59cf4b7
zxoho <348105852@qq.com>
2019-01-28 00:00:30 +0800

    角色服务service

---
+++ user-thrift-service/src/test/java/com/cn/userthriftservice/UserThriftServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cn.userthriftservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserThriftServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
