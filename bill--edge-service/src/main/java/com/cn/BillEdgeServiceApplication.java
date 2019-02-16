ebdcf9d9d38b13e57984e32524ad2f111995f3b4
zxoho <348105852@qq.com>
2019-02-17 03:43:05 +0800

    服务docker化

---
+++ bill--edge-service/src/main/java/com/cn/BillEdgeServiceApplication.java
@@ -0,0 +1,14 @@
+package com.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillEdgeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillEdgeServiceApplication.class, args);
+    }
+
+}
+
